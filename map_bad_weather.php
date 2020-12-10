c1e9f6a95af5e1ea5224c1bcf571915d499dd93d
nb341 <54728017+nb341@users.noreply.github.com>
2020-12-10 19:59:39 -0400

    Update map_bad_weather.php

--- map_bad_weather.php
+++ map_bad_weather.php
@@ -4,7 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="UTF-8">
 <title>Inclement Weather Notice</title>
-<script type="text/javascript" src=" https://maps.googleapis.com/maps/api/js?key=AIzaSyA5Tfpv2iAUjFHHhd__HFFgHJ3BtySGVIc&callback=initMap"></script>
+<script type="text/javascript" src=" https://maps.googleapis.com/maps/api/js?key='insert api key here'&callback=initMap"></script>
 <script type="text/javascript">
 //<![CDATA[
 
